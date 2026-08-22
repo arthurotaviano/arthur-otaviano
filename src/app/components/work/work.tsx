c68177684af34effec4270bba27be3823e6eb231
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-08-22 05:47:59 -0300

    feat: create work section

---
+++ src/app/components/work/work.tsx
@@ -0,0 +1,18 @@
+export function Work() {
+  return (
+    <section className='flex flex-col gap-5'>
+      <h3 className='text-mauve-950 font-medium'>Work</h3>
+      <p className='text-balance leading-normal'>
+        While I&apos;m building this section,{' '}
+        <a className='link' href='https://github.com/arthurotaviano' target='_blank'>
+          check my GitHub
+        </a>{' '}
+        to catch up on what I&apos;ve been working on lately. Also, feel free to{' '}
+        <a className='link' href='mailto:arthurotavianodev@gmail.com' target='_blank'>
+          reach out
+        </a>{' '}
+        anytime!
+      </p>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from './components/hero/hero'
 import { TechStack } from './components/tech-stack/tech-stack'
+import { Work } from './components/work/work'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <TechStack />
+      <Work />
     </>
   )
 }
