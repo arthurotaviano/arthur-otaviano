764cf0721f6cf93edca0594da6b80a0a316e1fd0
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-08-22 05:52:01 -0300

    style(footer): change font color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,5 @@
 export function Footer() {
-  return <footer className='text-sm'>Arthur Otaviano – {new Date().getFullYear()}</footer>
+  return (
+    <footer className='text-mauve-400 text-sm'>Arthur Otaviano – {new Date().getFullYear()}</footer>
+  )
 }
