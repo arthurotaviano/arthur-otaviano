526f18c3b6a2077b76083c359d96ed1e51788dac
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-09-02 15:02:40 -0300

    refactor(home): move data to respective components files

--- src/features/home/components/tech-stack.tsx
+++ src/features/home/components/tech-stack.tsx
@@ -1,4 +1,32 @@
-import { stack } from '../data/tech-stack'
+import type { IconType } from 'react-icons'
+import {
+  SiCss,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+  SiVercel,
+} from 'react-icons/si'
+
+type TechProps = {
+  name: string
+  icon: IconType
+}
+
+export const stack: TechProps[] = [
+  { name: 'React', icon: SiReact },
+  { name: 'Next.js', icon: SiNextdotjs },
+  { name: 'TypeScript', icon: SiTypescript },
+  { name: 'JavaScript', icon: SiJavascript },
+  { name: 'HTML', icon: SiHtml5 },
+  { name: 'CSS', icon: SiCss },
+  { name: 'Tailwind CSS', icon: SiTailwindcss },
+  { name: 'Git', icon: SiGit },
+  { name: 'Vercel', icon: SiVercel },
+]
 
 export function TechStack() {
   return (
--- src/features/home/components/work.tsx
+++ src/features/home/components/work.tsx
@@ -1,7 +1,22 @@
 import { CONTACT_LINKS } from '@/constants/contact'
 import { LuGlobe } from 'react-icons/lu'
 import { SiGithub } from 'react-icons/si'
-import { projects } from '../data/work'
+
+type ProjectProps = {
+  name: string
+  description: string
+  liveUrl?: string
+  repoUrl?: string
+}
+
+export const projects: ProjectProps[] = [
+  {
+    name: 'Larissa Machado',
+    description: 'Sr Graphic Designer portfolio.',
+    liveUrl: 'https://www.larissamachado.com.br/',
+    repoUrl: `${CONTACT_LINKS.GITHUB}/larissa-machado`,
+  },
+]
 
 export function Work() {
   return (
