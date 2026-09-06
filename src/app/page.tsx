a2c2c6ac5aae64e4a651880bffab4ee48adf9b68
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-09-06 15:12:46 -0300

    refactor: rename features to views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Hero } from '@/features/home/components/hero'
-import { TechStack } from '@/features/home/components/tech-stack'
-import { Work } from '@/features/home/components/work'
+import { Hero } from '@/views/home/components/hero'
+import { TechStack } from '@/views/home/components/tech-stack'
+import { Work } from '@/views/home/components/work'
 
 export default function Home() {
   return (
